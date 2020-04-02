ad24ea717f2459e6e7b2fbc6e71fbe20641d8086
rafalklat <rafalklat@quantexa.com>
2020-04-02 15:15:00 +0200

    extract time conversion to utils

--- stock_value.py
+++ stock_value.py
@@ -4,6 +4,7 @@
 import requests
 from bs4 import BeautifulSoup
 import datetime
+from utils import convert_epoch
 
 
 class StockValue:
@@ -13,14 +14,13 @@ class StockValue:
     # todo: decorator for printing time elapsed (sent in mail?)
     def get_bankier(self):
         address = 'https://www.bankier.pl/inwestowanie/profile/quote.html?symbol=' + self.symbol
-        print('address:', address)
         raw_site = requests.get(address).text.encode("utf-8")
         parsed_site = BeautifulSoup(raw_site, features="html.parser")
         return parsed_site
 
-    def find_values(self, parsed_site):
+    def get_values(self, parsed_site):
         """Return tuple (stock_value, trade_time)."""
-        
+
         trades = parsed_site.find_all("div", {'id': 'last-trade-' + self.symbol})
         if len(trades) > 1:
             # todo: what if >1 last-trade-symbol
@@ -28,11 +28,7 @@ class StockValue:
 
         stock_value = float(trades[0]['data-last'])
         trade_time_epoch = int(trades[0]['data-last-epoch'])
-        # todo: extract converter
-        trade_time = datetime.datetime.fromtimestamp(trade_time_epoch/1000).strftime('%c')
-
-        print('stock_value:', stock_value)
-        print('trade_time:', trade_time)
+        trade_time = convert_epoch(trade_time_epoch)
 
         return stock_value, trade_time
 
---
+++ utils.py
@@ -0,0 +1,7 @@
+import datetime
+
+
+def convert_epoch(epoch_time):
+    int_time = int(epoch_time)/1000
+    # todo: change format to something prettier
+    return datetime.datetime.fromtimestamp(int_time).strftime('%c')
