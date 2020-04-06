3cd9c590f4be948c65bc61e41d4de79c1732ef42
rafalklat <rafalklat@quantexa.com>
2020-04-06 12:51:13 +0200

    extract comparing 'global vs current' to email_sender

--- app/email_sender.py
+++ app/email_sender.py
@@ -1,6 +1,8 @@
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+
+from app.main import LOGIN, PASSWORD, MY_EMAIL
 from app.utils import calc_time
 
 
@@ -25,3 +27,23 @@ def send_email(login, password, recipient, subject, message):
     server.quit()
 
     del msg
+
+
+def compare_and_send(current_value, global_min, global_max):
+    if current_value > global_max:
+        subject = 'New global maximum'
+        message = f'Current_value: {current_value} > {global_max} (global_max)'
+        print(message)
+        send_email(login=LOGIN, password=PASSWORD, recipient=MY_EMAIL,
+                   subject=subject, message=message)
+
+    elif current_value < global_min:
+        subject = 'New global minimum'
+        message = f'Current_value: {current_value} < {global_min} (global_max)'
+        print(message)
+        send_email(login=LOGIN, password=PASSWORD, recipient=MY_EMAIL,
+                   subject=subject, message=message)
+
+    else:
+        message = f'Current_value: {current_value}.'
+        print(message + "; email not sent.")
--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from app.stock_value import StockValue
 from app.history_manager import HistoryManager
-from app.email_sender import send_email
+from app.email_sender import compare_and_send
 from app import utils
 import os
 
@@ -10,32 +10,17 @@ MY_EMAIL = os.environ.get('MY_GMAIL')
 
 
 def scheduled_function():
+    print("-"*30)
     current_value, bankier_time = get_current_stock()
-    mmm = HistoryManager()
-    history = mmm.get_history()
+    hm = HistoryManager()
+    history = hm.get_history()
 
     global_min = float(history['min'])
     global_max = float(history['max'])
 
-    if current_value > global_max:
-        subject = 'New global maximum'
-        message = f'Current_value: {current_value} > {global_max} (global_max)'
-        print(message)
-        send_email(login=LOGIN, password=PASSWORD, recipient=MY_EMAIL,
-                   subject=subject, message=message)
+    compare_and_send(current_value, global_min, global_max)
 
-    elif current_value < global_min:
-        subject = 'New global minimum'
-        message = f'Current_value: {current_value} < {global_min} (global_max)'
-        print(message)
-        send_email(login=LOGIN, password=PASSWORD, recipient=MY_EMAIL,
-                   subject=subject, message=message)
-
-    else:
-        message = f'Current_value: {current_value}.'
-        print(message + "; email not sent.")
-
-    mmm.save_new_values(current_value, bankier_time, utils.now())
+    hm.save_new_values(current_value, bankier_time, utils.now())
 
 
 def get_current_stock():
